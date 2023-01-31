910e3843151bb5039940eea9926dc38af53ca0da
Ethan <emcdaniel1624@gmail.com>
2023-01-31 00:04:24 -0700

    fixing instruction wording

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { api } from "../utils/api";
 const Home: NextPage = () => {
   const [value, setValue] = useState<string>("");
   const [noResponse, setNoResponse] = useState(true);
-  const [response, setResponse] = useState<string>("Enter a prompt to generate Regex")
+  const [response, setResponse] = useState<string>("Enter a prompt to generate a Regex")
   const { refetch } = api.generate.generate.useQuery(
     {
       prompt: value
@@ -37,7 +37,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Regex generator built with GPT3" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Regex<span className="text-[hsl(280,100%,70%)]">.AI</span>
