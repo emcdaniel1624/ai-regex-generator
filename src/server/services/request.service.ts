da65d8b45ce4ca5994070dd6122733cf5dddf643
Ethan <emcdaniel1624@gmail.com>
2023-02-01 11:06:28 -0700

    fixing error from build output

--- src/server/services/request.service.ts
+++ src/server/services/request.service.ts
@@ -2,11 +2,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import type { Prisma, Request } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import { prisma } from "../db";
 
 export const createRequest = async (input: Prisma.RequestCreateInput) => {
     return (await prisma.request.create({
         data: input
-    })) as Request;
+    }));
   };
